c4933293911cc17e62a89b1a8a535115ed231254
Artur Honzawa <arturh@gmail.com>
2017-08-27 10:24:55 +0200

    Use more elegant recursive solution to solve Euler031

--- src/main/scala/Euler031.scala
+++ src/main/scala/Euler031.scala
@@ -14,51 +14,25 @@ object Euler031 {
     println(s"solution: $solution")
   }
 
-  val coinValues = Array(1, 2, 5, 10, 20, 50, 100, 200)
-
   def solution: Int = {
-    val targetValue = 200
-    var result = 0
+    val coinValues: List[Int] = List(1, 2, 5, 10, 20, 50, 100, 200).reverse
+    numberOfWaysToMakeChange(200, coinValues)
+  }
 
-    for (a <- 0 to (targetValue / coinValues(0))) {
-      val targetValueB = targetValue - a * coinValues(0)
-      for (b <- 0 to (targetValueB / coinValues(1))) {
-
-        val targetValueC = targetValueB - b * coinValues(1)
-        for (c <- 0 to (targetValueC / coinValues(2))) {
-
-          val targetValueD = targetValueC - c * coinValues(2)
-          for (d <- 0 to (targetValueD / coinValues(3))) {
-
-            val targetValueE = targetValueD - d * coinValues(3)
-            for (e <- 0 to (targetValueE / coinValues(4))) {
-
-              val targetValueF = targetValueE - e * coinValues(4)
-              for (f <- 0 to (targetValue / coinValues(5))) {
-
-                val targetValueG = targetValueF - f * coinValues(5)
-                for (g <- 0 to (targetValueG / coinValues(6))) {
-
-                  val targetValueH = targetValueG - g * coinValues(6)
-                  for (h <- 0 to (targetValueH / coinValues(7))) {
-                    val value =
-                      Array(a, b, c, d, e, f, g, h)
-                        .zip(coinValues)
-                        .map { case (quantity, coinValue) => quantity * coinValue }
-                        .sum
-                    
-                    if (value == targetValue) {
-                      println(Array(a, b, c, d, e, f, g, h).mkString(" "))
-                      result += 1
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
+  def numberOfWaysToMakeChange(target: Int, coinValues: List[Int]): Int = {
+    coinValues match {
+      case Nil => if (target == 0) {
+        1
+      } else {
+        0
       }
+      case coinValue :: rest =>
+        (0 to target / coinValue)
+          .map {
+            (coinQuantity: Int) =>
+              val newTarget = target - coinQuantity * coinValue
+              numberOfWaysToMakeChange(newTarget, rest)
+          }.sum
     }
-    result
   }
 }
